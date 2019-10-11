909162beed8ea95b5aec743a945c505066a5bb96
Aleksandr <38788523+sansanich83@users.noreply.github.com>
2019-10-11 20:43:44 +0300

    добавляет скролл, акордеон

--- source/js/main.js
+++ source/js/main.js
@@ -32,3 +32,52 @@ document.addEventListener('keydown', function (evt) {
     bodyHolder.classList.remove('feedback--popup--show');
   }
 });
+
+// собираем все якоря; устанавливаем время анимации и количество кадров
+var anchors = [].slice.call(document.querySelectorAll('a[href*="#"]'));
+var animationTime = 300;
+var framesCount = 20;
+
+anchors.forEach(function (item) {
+  // каждому якорю присваиваем обработчик события
+  item.addEventListener('click', function (e) {
+    // убираем стандартное поведение
+    e.preventDefault();
+
+    // для каждого якоря берем соответствующий ему элемент и определяем его координату Y
+    var coordY = document.querySelector(item.getAttribute('href')).getBoundingClientRect().top + window.pageYOffset;
+
+    // запускаем интервал, в котором
+    var scroller = setInterval(function () {
+      // считаем на сколько скроллить за 1 такт
+      var scrollBy = coordY / framesCount;
+
+      // если к-во пикселей для скролла за 1 такт больше расстояния до элемента
+      // и дно страницы не достигнуто
+      if (scrollBy > window.pageYOffset - coordY && window.innerHeight + window.pageYOffset < document.body.offsetHeight) {
+        // то скроллим на к-во пикселей, которое соответствует одному такту
+        window.scrollBy(0, scrollBy);
+      } else {
+        // иначе добираемся до элемента и выходим из интервала
+        window.scrollTo(0, coordY);
+        clearInterval(scroller);
+      }
+      // время интервала равняется частному от времени анимации и к-ва кадров
+    }, animationTime / framesCount);
+  });
+});
+
+var accordeonBtns = document.querySelectorAll('.contacts__section-button');
+var i;
+
+for (i = 0; i < accordeonBtns.length; i++) {
+
+  accordeonBtns[i].addEventListener('click', function () {
+    // if (!(this.classList.contains('contacts__section-button--closed'))) {
+    //   this.classList.add('contacts__section-button--closed');
+    // }
+    this.classList.toggle('contacts__section-button--closed');
+    this.parentElement.nextElementSibling.classList.toggle('contacts__accordion-item--closed');
+
+  });
+}
